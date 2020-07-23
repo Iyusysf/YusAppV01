865a3e460eef3a63a0f86ea7525a3f6255038d09
Iyusysf <42061520+Iyusysf@users.noreply.github.com>
2020-07-23 18:36:55 +0700

    edit jasonmanifest jason manifest

--- src/components/AppBar/styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-export default makeStyles(theme => ({
-  toolbar: {
-    justifyContent: "space-between"
-  },
-  menuBtn: {
-    color: "#fff"
-  }
-}));
--- src/pages/private/chat/styles/list.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-export default makeStyles(theme => ({
-  fab: {
-    position: "fixed",
-    right: 15,
-    bottom: 15
-  }
-}));
